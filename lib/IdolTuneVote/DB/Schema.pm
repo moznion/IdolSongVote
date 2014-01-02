4c62874f292c1de95d9b0204227bfd1d9e33351c
moznion <moznion@gmail.com>
2014-01-03 01:22:19 +0900

    Define DB schema

--- lib/IdolTuneVote/DB/Schema.pm
+++ lib/IdolTuneVote/DB/Schema.pm
@@ -8,9 +8,15 @@ use Teng::Schema::Declare;
 base_row_class 'IdolTuneVote::DB::Row';
 
 table {
-    name 'member';
-    pk 'id';
-    columns qw(id name);
+    name 'serial_numbers';
+    pk 'serial_number';
+    columns qw(serial_number is_used);
 };
+
+table {
+    name 'tunes';
+    pk 'id';
+    columns qw(id title polled);
+}
 
 1;
