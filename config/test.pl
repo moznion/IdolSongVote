963ac61fb39a2114ff2702ecd3bb463b0bdffd98
moznion <moznion@gmail.com>
2014-01-04 00:58:39 +0900

    Update db configurations.

--- config/test.pl
+++ config/test.pl
@@ -1,12 +1,8 @@
-use File::Spec;
-use File::Basename qw(dirname);
-my $basedir = File::Spec->rel2abs(File::Spec->catdir(dirname(__FILE__), '..'));
-my $dbpath = File::Spec->catfile($basedir, 'db', 'test.db');
 +{
     'DBI' => [
-        "dbi:SQLite:dbname=$dbpath", '', '',
+        "dbi:mysql:TestIdolSongVote", 'root', '',
         +{
-            sqlite_unicode => 1,
+            'mysql_enable_utf8' => 1,
         }
     ],
 };
