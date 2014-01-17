2ee4370123cbd03ebcfc391adceca30b3ebbace7
moznion <moznion@gmail.com>
2014-01-17 14:37:28 +0900

    Fix #20

--- lib/IdolSongVote/Util.pm
+++ lib/IdolSongVote/Util.pm
@@ -15,11 +15,6 @@ sub embed_content_to_base_html {
 
 sub _load_base_html {
     open my $fh, '<', "$FindBin::Bin/tmpl/base.html";
-    my $html = '';
-    while (my $line = <$fh>) {
-        $html .= $line;
-    }
-
-    return $html;
+    do { local $/; <$fh>; }
 }
 1;
